d1565be0453728f5a12dc62dbad37dec0607655b
Daniel <hyun@Daniels-MacBook-Air.local>
2023-08-25 17:25:09 -0400

    initial commit

---
+++ server/config/env/production/database.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+	connection: {
+	  client: 'postgres',
+	  connection: {
+		connectionString: env('DATABASE_URL'),
+        host: env('DATABASE_HOST', 'containers-us-west-95.railway.app'),
+        port: env.int('DATABASE_PORT', 6823),
+        database: env('DATABASE_NAME', 'railway'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', '17sVqjIvdShTZg8krYEz'),
+		ssl: env.bool(true),
+	  },
+	},
+  });
